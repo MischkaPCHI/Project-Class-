f67bcf661b8163f88562af0ad36a904308f971e5
MischkaPCHI <michellejung4002@gmail.com>
2024-02-16 12:54:18 +0100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+const heightInput = document.querySelector('#your-height');
+const weightInput = document.querySelector('#your-weight');
+
+let userInputHeight;
+const inputFieldHeight = (event) => {
+    userInputHeight = event.target.value;
+    console.log(userInputHeight);
+}
+heightInput.addEventListener('input', inputFieldHeight);
+
+let userInputWeight;
+const inputFieldWeight = (event) => {
+    userInputWeight = event.target.value;
+    alert("Олень!")
+}
+weightInput.addEventListener('input', inputFieldWeight);
+
+const createButtonElement = document.querySelector("button");
+
+const bmiElement = document.querySelector('#result1');
+const resultElement = document.querySelector('#result2');
+
+const submitButtonHandler = (event) => {
+    event.preventDefault();
+    let bmi = userInputWeight/((userInputHeight/100)**2);
+    bmiElement.textContent = bmi;
+    let result;
+    if(bmi>18.5 && bmi<25){
+        result  = "Norm";
+        }
+    if(bmi<18.5){
+        result = "Lower than norm"
+        }
+    if(bmi>25){
+        result = "Higher than norm"
+        }
+    resultElement.textContent = result;
+}
+
+createButtonElement.addEventListener("click", submitButtonHandler);
