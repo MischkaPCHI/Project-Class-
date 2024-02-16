bd75ad8d919df3b6de685b3c24a7a66148677e32
AnnyMee <151825850+AnnyMee@users.noreply.github.com>
2024-02-16 15:28:00 +0100

    yolo!

--- scripts/index.js
+++ scripts/index.js
@@ -3,38 +3,65 @@ const weightInput = document.querySelector('#your-weight');
 
 const bmiElement = document.querySelector('#result1');
 const resultElement = document.querySelector('#result2');
-const createButtonElement = document.querySelector("button");
+const createButtonElement = document.querySelector("#calculate");
+const resetButtonElement = document.querySelector("#clear");
 
 let userInputHeight;
 let userInputWeight;
 
+let bmi;
+let result;
+
 const inputFieldHeight = (event) => {
     userInputHeight = event.target.value;
-    console.log(userInputHeight);
+    if(isNaN(userInputHeight)){
+        showAlertNaN();
+    }
 }
 heightInput.addEventListener('input', inputFieldHeight);
 
-
 const inputFieldWeight = (event) => {
     userInputWeight = event.target.value;
-    // alert("Олень!")
+    if(isNaN(userInputWeight)){
+        showAlertNaN();
+    }
 }
 weightInput.addEventListener('input', inputFieldWeight);
 
+const showAlertNaN = () => {
+    alert("Олень! Вводи рост и вес в цифрах!");
+}
+
 const submitButtonHandler = (event) => {
     event.preventDefault();
-    let bmi = userInputWeight/((userInputHeight/100)**2);
+    if ((userInputHeight===undefined && userInputWeight===undefined) || (userInputHeight===undefined || userInputWeight===undefined)){
+        showAlertEmpty();
+    }else{
+    bmi = userInputWeight/((userInputHeight/100)**2);
     bmiElement.textContent = bmi;
-    let result;
+    result;
     if(bmi>18.5 && bmi<25){
         result  = "Norm";
         }
     if(bmi<18.5){
-        result = "Lower than norm"
+        result = "Lower than norm";
         }
     if(bmi>25){
-        result = "Higher than norm"
+        result = "Higher than norm";
         }
     resultElement.textContent = result;
+    }
 }
 createButtonElement.addEventListener("click", submitButtonHandler);
+
+const resetButtonHandler = () => {
+    bmi = "";
+    bmiElement.textContent = bmi;
+    result = "";
+    resultElement.textContent = result;
+}
+resetButtonElement.addEventListener("click", resetButtonHandler);
+
+const showAlertEmpty = () => {
+    alert("Лось! Вводи  И рост И вес!!");
+}
